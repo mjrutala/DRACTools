9ca4298f3d4a1e497f13bf9815685a98371d0b3f
mjrutala <mrutala@bu.edu>
2026-04-09 16:12:42 -0600

    Initial commit of convert_requirements.py, a command-line enabled script to convert existing requirements.txt to DRAC-style requirements

---
+++ convert_requirements.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr  9 14:58:02 2026
+
+@author: mrutala
+"""
+
+def pip_install(req_file, download_dir):
+    import subprocess
+    import sys
+    import glob
+    
+    if download_dir[-1] != '/':
+        download_dir += '/'
+    output_file = req_file.split('.')[0] + 'DRAC.txt'
+    
+    def pip_install(req_line):
+        subprocess.check_call([sys.executable, '-m', 'pip', 'install', req_line])
+    
+    
+    with open(req_file) as f:
+        requirements = [line.strip() for line in f if line.strip() and not line.startswith('#')]
+    
+    for req in requirements:
+        try:
+            subprocess.check_call([sys.executable, '-m', 'pip', 'install', '--no-index', req])
+        except:
+            subprocess.check_call([sys.executable, '-m', 'pip', 'download', '--no-deps', req, '-d', download_dir])
+            path = glob.glob(download_dir + '/*{}*'.format(req.split('=')[0]))
+            subprocess.check_call([sys.executable, '-m', 'pip', 'install', path[0]])
+    
+    # Generate a requirements.txt
+    subprocess.check_call([sys.executable, '-m', 'pip', 'freeze', '--local', '>', output_file])
+    
+if __name__ == "__main__":
+    import argparse
+    parser = argparse.ArgumentParser(description="Convert pip requirements for DRAC use.")
+    parser.add_argument("requirements-file", help="The original, non-DRAC requirements file (e.g., requirements.txt)", default='requirements.txt')
+    parser.add_argument("-d", "--download-dir", help="Location for pip files that aren't on DRAC to be downloaded.", default='./')
+    args = parser.parse_args()
+    
+    pip_install(args.requirements_file, args.download_dir)
